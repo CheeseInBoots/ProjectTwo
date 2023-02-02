2e5ded42305e5047f0469f1f14e77bb486012a87
Yasin <yasinwinther@gmail.com>
2023-02-02 21:55:01 +0000

    "Creates an method that returns 10"

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static int passByValue(int a ){
+      a = 10;
+
+      return a;
+    }
 }
